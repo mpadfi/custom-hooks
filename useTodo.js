00e0250f30e9b88dcaed6c4f7cd39a5f3540ccb6
mpadfi <manuel.polaina@adfi.mobi>
2023-03-10 10:16:48 +0100

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,44 @@
+import { useReducer, useEffect } from 'react';
+import { todoReducer } from '../08-useReducer/todoReducer';
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || []
+}
+
+export const useTodo = () => {
+
+  const [todos, dispatch ] = useReducer( todoReducer, [], init )
+
+  const todosCount = todos.length
+  const pendingCount = todos.filter( todo => todo.done === false).length
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos]);
+
+  const handleNewTodo = ( todo ) => {
+    const action = {
+      type: '[TODO] Add Todo',
+      payload: todo
+    }
+    dispatch( action )
+  }
+
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: '[TODO] Remove Todo',
+      payload: id
+    })
+  }
+  
+  const handleToggleTodo = (id) => {
+    dispatch({
+      type: '[TODO] Toogle Todo',
+      payload: id
+    })
+  }
+
+  return {
+    todos, handleNewTodo, handleDeleteTodo, handleToggleTodo, todosCount, pendingCount
+  }
+}
